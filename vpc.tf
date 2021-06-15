8d23b50669f3c7074b5e4599de6a42c69c9218d6
leon0525 <leon0525@gmail.com>
2021-06-15 13:36:45 +0000

    change region

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,7 @@ module "vpc" {
     source  = "app.terraform.io/leonlin-training/network/google//modules/vpc"
     version = "3.2.2"
 
+
     project_id   = "var.project"
     network_name = "gaurav-network"
 
